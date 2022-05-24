f54f2639918afba062ffb55852446e5007416d0c
abinash889 <abinash889@gmail.com>
2022-05-24 18:20:22 +0530

    login

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,23 @@
+<div class="sidebar-wrapper" data-simplebar="true">
+            <div class="sidebar-header">
+                <div>
+                    <img src="assets/images/logo-icon.png" class="logo-icon" alt="logo icon">
+                </div>
+                <div>
+                    <h4 class="logo-text">Aeroindia</h4>
+                </div>
+                <!-- <div class="toggle-icon ms-auto"><i class='bx bx-first-page'></i>
+                </div> -->
+            </div>
+            <!--navigation-->
+            <ul class="metismenu" id="menu">
+                <li>
+                    <a href="javascript:;" class="">
+                        <div class="parent-icon"><i class='bx bx-home'></i>
+                        </div>
+                        <div class="menu-title">Dashboard</div>
+                    </a>
+                </li>
+            </ul>
+            <!--end navigation-->
+        </div>
