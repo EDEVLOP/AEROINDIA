faf2c7ea4c43f0316099a553fe9ce601b96e5f3e
abinash889 <abinash889@gmail.com>
2022-05-26 19:56:04 +0530

    database migrate

--- database/migrations/2022_05_26_142448_create_tbl_city_table.php
+++ database/migrations/2022_05_26_142448_create_tbl_city_table.php
@@ -15,12 +15,12 @@ class CreateTblCityTable extends Migration
     {
         Schema::create('tbl_city', function (Blueprint $table) {
             $table->integer('id', true);
+            $table->string('vch_cityname', 255);
             $table->integer('INT_StateID');
-            $table->string('vch_citycode', 255)->nullable();
-            $table->string('vch_cityname', 255)->nullable();
-            $table->string('vch_status', 255)->nullable();
-            $table->dateTime('DT_createdon')->useCurrent();
-            $table->dateTime('DT_updatedon')->useCurrent();
+            $table->string('vch_citycode', 11);
+            $table->string('vch_status', 11)->default('Active');
+            $table->timestamp('DT_createdon')->useCurrent();
+            $table->timestamp('DT_updatedon')->useCurrent();
         });
     }
 
